dc031775bab93a9475bfece7efdab4f3d8dbf6dc
job_machine <123@ya.ru>
2019-02-28 14:57:29 +0300

    28.02.2019

--- AngularTutorial/src/app/sections/section-orders/section-orders.component.ts
+++ AngularTutorial/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -9,7 +10,46 @@ export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
 
-  orders: Order[];
+  orders: Order[] = [
+    {
+      id: 1, customer:
+        { id: 1, name: 'Main', state: 'CO', email: 'mail@mail.com' },
+      total: 230, placed: new Date(2019, 2, 20), fulfilled: new Date(2019, 2, 21),
+      status: 'Completed'
+    },
+    {
+      id: 2, customer:
+        { id: 1, name: 'Main', state: 'CO', email: 'mail@mail.com' },
+      total: 230, placed: new Date(2019, 2, 20), fulfilled: new Date(2019, 2, 21),
+      status: 'Completed'
+    },
+    {
+      id: 3, customer:
+        { id: 1, name: 'Main', state: 'CO', email: 'mail@mail.com' },
+      total: 230, placed: new Date(2019, 2, 20), fulfilled: new Date(2019, 2, 21),
+      status: 'Completed'
+    },
+    {
+      id: 4, customer:
+        { id: 1, name: 'Main', state: 'CO', email: 'mail@mail.com' },
+      total: 230, placed: new Date(2019, 2, 20), fulfilled: new Date(2019, 2, 21),
+      status: 'Completed'
+    },
+    {
+      id: 5, customer:
+        { id: 1, name: 'Main', state: 'CO', email: 'mail@mail.com' },
+      total: 230, placed: new Date(2019, 2, 20), fulfilled: new Date(2019, 2, 21),
+      status: 'Completed'
+    },
+    {
+      id: 6, customer:
+        { id: 1, name: 'Main', state: 'CO', email: 'mail@mail.com' },
+      total: 230, placed: new Date(2019, 2, 20), fulfilled: new Date(2019, 2, 21),
+      status: 'Completed'
+    },
+
+
+  ]
 
   ngOnInit() {
   }
